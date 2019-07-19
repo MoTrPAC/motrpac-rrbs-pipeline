d8645e151fb76781adcb59e493500885d6dcc048
Archana Raja <araja7@stanford.edu>
2019-07-19 12:42:39 -0700

    Added how I submit the pipeline on GCP

--- run_rrbs.sh
+++ run_rrbs.sh
@@ -3,3 +3,10 @@ java -Dconfig.file=google_prod_PAPI.conf -jar ../../tools/cromwell-40.jar run rr
 
 ###Compute checksums
 for i in `ls checksum/sinai/rrbs/*.json`;do echo java -Dconfig.file=google_prod_PAPI.conf -jar ../../tools/cromwell-40.jar run checksum/compute_md5sum.wdl -i $i "&";done >>checksums_rrbs_sinai.sh
+
+java -Dconfig.file=google_prod_PAPI.conf -jar ../../tools/cromwell-40.jar run rrbs_pipeline_scatter.wdl -i input_json/pilot/pilot_list_rrbs.json
+
+Running on GCP
+
+nohup java -Dconfig.file=google_prod_PAPI.conf -jar ../tools/cromwell-40.jar run rrbs_pipeline_scatter.wdl -i input_json/pilot_list_rrbs.json &>logs/test_pilot.log &
+nohup java -Dconfig.file=google_prod_PAPI.conf -jar ../tools/cromwell-40.jar run rrbs_pipeline_scatter.wdl -i input_json/rrbs_batch1ac_rrbs.json &>logs/rrbs_pass1a_sinai_batch1ac.log &
